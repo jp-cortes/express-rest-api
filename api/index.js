85960adf36a680100a9c1ca929af73ab08b9ffb2
JP <alias99002@outlook.com>
2023-06-07 13:09:34 -0500

    updating url

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ require('dotenv').config({path: './.env'});// first read the  .env variables
 
 const cors = require('cors');// validate routes
 const express = require('express');
+const serverless = require('serverless-http');
 const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler, ormErrorhandler } = require('./middlewares/error.handler');
 const { checkApiKey } = require('./middlewares/auth.handler');
@@ -9,26 +10,26 @@ const { checkApiKey } = require('./middlewares/auth.handler');
 
 
 const app = express();
-const port = process.env.PORT;
+// const port = process.env.PORT;
 
 app.use(express.json());
 
 // example for specific url
-const whitelist = [`${process.env.SERVER}`, 'http://localhost:3000'];
+// const whitelist = [`${process.env.SERVER}`, 'http://localhost:3000'];
 
-const corsOptionsDelegate = (req, callback) => {
-  let corsOptions;
-if (whitelist.indexOf(req.header('Origin')) !== -1) {
-  corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-} else {
-  corsOptions = { origin: false } // disable CORS for this request
-}
-callback(null, corsOptions) // callback expects two parameters: error and options
-}
+// const corsOptionsDelegate = (req, callback) => {
+//   let corsOptions;
+// if (whitelist.indexOf(req.header('Origin')) !== -1) {
+//   corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+// } else {
+//   corsOptions = { origin: false } // disable CORS for this request
+// }
+// callback(null, corsOptions) // callback expects two parameters: error and options
+// }
 
-app.use(cors(corsOptionsDelegate)); //access for the specific url
+// app.use(cors(corsOptionsDelegate)); //access for the specific url
 
-// app.use(cors()); //access for all urls
+app.use(cors()); //access for all urls
 
 //user authentication & authorization
 require('./utils/auth');
@@ -40,12 +41,15 @@ app.get('/',checkApiKey, (req, res) => {
 
 routerApi(app);
 
+
 app.use(logErrors);
 app.use(ormErrorhandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
 
-app.listen(port, () => {
-  console.log(`Listening on port http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Listening on port http://localhost:${port}`);
+// });
+ module.exports = app;
+ module.exports.handler = serverless(app);
