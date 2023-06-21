5f18ba12b01f19628bb0f9131a6f92a4061c28e6
Juan Cortes <105751301+jp-cortes@users.noreply.github.com>
2023-06-21 14:39:49 -0500

    Update customers.router.js

--- api/routes/customers.router.js
+++ api/routes/customers.router.js
@@ -34,7 +34,7 @@ async (req, res, next) => {
   try {
     const { id } = req.params;
     const customer = await service.findById(id);
-    res.status(404).json(customer);
+    res.status(200).json(customer);
   } catch (error) {
     next(error);
   }
