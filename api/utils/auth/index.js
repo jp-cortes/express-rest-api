ef696fc068afb5a218c0ffdae69fdb16a4c2308d
JP <alias99002@outlook.com>
2023-06-24 05:48:06 -0500

    folder api changed to src

--- api/utils/auth/index.js
+++
@@ -1,7 +0,0 @@
-const passport = require('passport');
-
-const LocalStrategy = require('./strategies/local.strategy');
-const JwtStrategy = require('./strategies/jwt.startegy');
-
-passport.use(LocalStrategy);
-passport.use(JwtStrategy);
