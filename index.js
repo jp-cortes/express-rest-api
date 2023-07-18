91292581bf1891f7f046fd3f7e01b75b7a895d0e
JP <alias99002@outlook.com>
2023-07-18 09:33:32 -0500

    update index.js main file

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { logErrors, errorHandler, boomErrorHandler, ormErrorhandler } = require('
 
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
