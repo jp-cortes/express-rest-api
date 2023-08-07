42179b801e6ec28da4bb643220482d2e20301c0a
JP <alias99002@outlook.com>
2023-08-07 14:32:45 -0500

    add relatio N:N order-products

--- api/graphql/order.resolvers.js
+++ api/graphql/order.resolvers.js
@@ -1,9 +1,27 @@
+const { checkJwtGql } = require('../utils/checkJwtGql');
+const { checkRolesGql } = require('../utils/checkRolesGql');
+
+
 const OrderService = require('../services/orders.services');
 const service = new OrderService();
 
-const  addOrder = async(_, { dto }) => {
+const getOrders = async(_, context) => {
+  const user = await checkJwtGql(context);
+  checkRolesGql(user, 'admin');
+  const orders = await service.find({});
+  return orders;
+}
+
+const  addOrder = async(_, { dto }, context) => {
+  await checkJwtGql(context);
   const newCustomer = await service.create(dto);
   return newCustomer;
 }
 
-module.exports = { addOrder };
+const  addProductToOrder = async(_, { dto }, context) => {
+  await checkJwtGql(context);
+  const item = await service.addItem(dto);
+  return item;
+}
+
+module.exports = { getOrders, addOrder, addProductToOrder };
--- api/graphql/resolvers.js
+++ api/graphql/resolvers.js
@@ -2,7 +2,7 @@ const { getProduct, getProducts, addProduct, updateProduct, deleteProduct, getPr
 const { getCategories, getCategory, addCategory, updateCategory, deleteCategory } = require('./category.resolvers');
 const { addCustomer, updateCustomer, deleteCustomer } = require('./customer.resolvers');
 const { login } = require('./auth.resolvers');
-const { addOrder } = require('./order.resolvers');
+const { getOrders, addOrder, addProductToOrder } = require('./order.resolvers');
 const { RegularExpression } = require('graphql-scalars');
 
 const CategoryNameType = new RegularExpression('CategoryNameType', /^[a-zA-Z0-9]{3,8}$/);
@@ -12,7 +12,8 @@ const resolvers = {
     product: getProduct,
     products: getProducts,
     category: getCategory,
-    categories: getCategories
+    categories: getCategories,
+    orders: getOrders
   },
   Mutation: {
     addProduct,
@@ -26,6 +27,7 @@ const resolvers = {
     updateCustomer,
     deleteCustomer,
     addOrder,
+    addItem: addProductToOrder
   },
   CategoryNameType,
   Category: {
